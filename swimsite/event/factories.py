7004d23e88c0a2f10f554ab2b1b451e0b8aba8c6
kanata3319 <brave3319@gmail.com>
2022-12-29 19:55:12 +0900

    初期データ作成&タイム表示処理の修正

--- swimsite/event/factories.py
+++ swimsite/event/factories.py
@@ -2,16 +2,31 @@ import factory
 from event import models as e_models
 
 
+class PlayerFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = e_models.Player
+
+
 class CompetitionFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = e_models.Competition
-        
+
 
 class IndividualEventFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = e_models.IndividualEvent
 
 
+class IndividualEventResultFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = e_models.IndividualEventResult
+
+
 class RelayEventFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = e_models.RelayEvent
+
+
+class RelayEventResultFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = e_models.RelayEventResult
